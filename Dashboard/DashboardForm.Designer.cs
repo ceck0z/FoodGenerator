c5259f0be0da5816d6911ce39fd7e05a64793117
ceck0z <ceck0z@gmail.com>
2019-12-22 01:42:43 +0200

    Initial Commit

---
+++ Dashboard/DashboardForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Dashboard
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.bulb1 = new System.Windows.Forms.Panel();
+            this.bulb2 = new System.Windows.Forms.Panel();
+            this.bulb4 = new System.Windows.Forms.Panel();
+            this.bulb3 = new System.Windows.Forms.Panel();
+            this.bulb8 = new System.Windows.Forms.Panel();
+            this.bulb7 = new System.Windows.Forms.Panel();
+            this.bulb6 = new System.Windows.Forms.Panel();
+            this.bulb5 = new System.Windows.Forms.Panel();
+            this.bulb9 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(67)))), ((int)(((byte)(92)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.bulb9);
+            this.panel1.Controls.Add(this.bulb8);
+            this.panel1.Controls.Add(this.bulb7);
+            this.panel1.Controls.Add(this.bulb4);
+            this.panel1.Controls.Add(this.bulb6);
+            this.panel1.Controls.Add(this.bulb3);
+            this.panel1.Controls.Add(this.bulb5);
+            this.panel1.Controls.Add(this.bulb2);
+            this.panel1.Controls.Add(this.bulb1);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 453);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(67)))), ((int)(((byte)(92)))));
+            this.panel2.Location = new System.Drawing.Point(142, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(659, 41);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(3, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.Button2_MouseEnter);
+            this.button2.MouseHover += new System.EventHandler(this.Button2_MouseHover);
+            this.button2.MouseLeave += new System.EventHandler(this.Button2_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(3, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.Button3_MouseEnter);
+            this.button3.MouseHover += new System.EventHandler(this.Button3_MouseHover);
+            this.button3.MouseLeave += new System.EventHandler(this.Button3_MouseLeave);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(3, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.Button4_MouseEnter);
+            this.button4.MouseHover += new System.EventHandler(this.Button4_MouseHover);
+            this.button4.MouseLeave += new System.EventHandler(this.Button4_MouseLeave);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(3, 206);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 35);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.MouseEnter += new System.EventHandler(this.Button5_MouseEnter);
+            this.button5.MouseHover += new System.EventHandler(this.Button5_MouseHover);
+            this.button5.MouseLeave += new System.EventHandler(this.Button5_MouseLeave);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(3, 366);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(112, 35);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.MouseEnter += new System.EventHandler(this.Button9_MouseEnter);
+            this.button9.MouseHover += new System.EventHandler(this.Button9_MouseHover);
+            this.button9.MouseLeave += new System.EventHandler(this.Button9_MouseLeave);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(3, 326);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(112, 35);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.MouseEnter += new System.EventHandler(this.Button8_MouseEnter);
+            this.button8.MouseHover += new System.EventHandler(this.Button8_MouseHover);
+            this.button8.MouseLeave += new System.EventHandler(this.Button8_MouseLeave);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(3, 246);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 35);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.MouseEnter += new System.EventHandler(this.Button6_MouseEnter);
+            this.button6.MouseHover += new System.EventHandler(this.Button6_MouseHover);
+            this.button6.MouseLeave += new System.EventHandler(this.Button6_MouseLeave);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(3, 286);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 35);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.MouseEnter += new System.EventHandler(this.Button7_MouseEnter);
+            this.button7.MouseHover += new System.EventHandler(this.Button7_MouseHover);
+            this.button7.MouseLeave += new System.EventHandler(this.Button7_MouseLeave);
+            // 
+            // bulb1
+            // 
+            this.bulb1.Location = new System.Drawing.Point(121, 46);
+            this.bulb1.Name = "bulb1";
+            this.bulb1.Size = new System.Drawing.Size(10, 35);
+            this.bulb1.TabIndex = 2;
+            // 
+            // bulb2
+            // 
+            this.bulb2.Location = new System.Drawing.Point(121, 86);
+            this.bulb2.Name = "bulb2";
+            this.bulb2.Size = new System.Drawing.Size(10, 35);
+            this.bulb2.TabIndex = 3;
+            // 
+            // bulb4
+            // 
+            this.bulb4.Location = new System.Drawing.Point(121, 166);
+            this.bulb4.Name = "bulb4";
+            this.bulb4.Size = new System.Drawing.Size(10, 35);
+            this.bulb4.TabIndex = 5;
+            // 
+            // bulb3
+            // 
+            this.bulb3.Location = new System.Drawing.Point(121, 126);
+            this.bulb3.Name = "bulb3";
+            this.bulb3.Size = new System.Drawing.Size(10, 35);
+            this.bulb3.TabIndex = 4;
+            // 
+            // bulb8
+            // 
+            this.bulb8.Location = new System.Drawing.Point(121, 326);
+            this.bulb8.Name = "bulb8";
+            this.bulb8.Size = new System.Drawing.Size(10, 35);
+            this.bulb8.TabIndex = 9;
+            // 
+            // bulb7
+            // 
+            this.bulb7.Location = new System.Drawing.Point(121, 286);
+            this.bulb7.Name = "bulb7";
+            this.bulb7.Size = new System.Drawing.Size(10, 35);
+            this.bulb7.TabIndex = 8;
+            // 
+            // bulb6
+            // 
+            this.bulb6.Location = new System.Drawing.Point(121, 246);
+            this.bulb6.Name = "bulb6";
+            this.bulb6.Size = new System.Drawing.Size(10, 35);
+            this.bulb6.TabIndex = 7;
+            // 
+            // bulb5
+            // 
+            this.bulb5.Location = new System.Drawing.Point(121, 206);
+            this.bulb5.Name = "bulb5";
+            this.bulb5.Size = new System.Drawing.Size(10, 35);
+            this.bulb5.TabIndex = 6;
+            // 
+            // bulb9
+            // 
+            this.bulb9.Location = new System.Drawing.Point(121, 366);
+            this.bulb9.Name = "bulb9";
+            this.bulb9.Size = new System.Drawing.Size(10, 35);
+            this.bulb9.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(132)))), ((int)(((byte)(227)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(3, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseLeave += new System.EventHandler(this.Button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.Button1_MouseHover);
+            this.button1.MouseEnter += new System.EventHandler(this.Button1_MouseEnter);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(49)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "DashboardForm";
+            this.Text = "Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel bulb1;
+        private System.Windows.Forms.Panel bulb9;
+        private System.Windows.Forms.Panel bulb8;
+        private System.Windows.Forms.Panel bulb7;
+        private System.Windows.Forms.Panel bulb4;
+        private System.Windows.Forms.Panel bulb6;
+        private System.Windows.Forms.Panel bulb3;
+        private System.Windows.Forms.Panel bulb5;
+        private System.Windows.Forms.Panel bulb2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Dashboard/DashboardForm.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Dashboard
+{
+    public partial class DashboardForm : Form
+    {
+        public DashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Button1 Events and Actions
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button1, bulb1);            
+        }
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button1);
+        }
+        private void Button1_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button1);
+        }
+
+        private void Button1_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button1);
+        }
+
+        #endregion
+
+        #region Button2 Events and Actions
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button2, bulb2);
+        }
+
+        private void Button2_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button2);
+        }
+        private void Button2_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button2);
+        }
+
+        private void Button2_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button2);
+        }
+        #endregion
+
+        #region Button3 Events and Actions
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button3, bulb3);
+        }
+
+        private void Button3_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button3);
+        }
+        private void Button3_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button3);
+        }
+
+        private void Button3_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button3);
+        }
+
+        #endregion
+
+        #region Button4 Events and Actions
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button4, bulb4);
+        }
+
+        private void Button4_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button4);
+        }
+        private void Button4_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button4);
+        }
+
+        private void Button4_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button4);
+        }
+        #endregion
+
+        #region Button5 Events and Actions
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button5, bulb5);
+        }
+        private void Button5_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button5);
+        }
+        private void Button5_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button5);
+        }
+
+        private void Button5_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button5);
+        }
+
+        #endregion
+
+        #region Button6 Events and Actions
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button6, bulb6);
+        }
+
+        private void Button6_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button6);
+        }
+        private void Button6_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button6);
+        }
+
+        private void Button6_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button6);
+        }
+
+        #endregion
+
+        #region Button7 Events and Actions
+        private void button7_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button7, bulb7);
+        }
+        private void Button7_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button7);
+        }
+        private void Button7_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button7);
+        }
+
+        private void Button7_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button7);
+        }
+
+        #endregion
+
+        #region Button8 Events and Actions
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button8, bulb8);
+        }
+
+        private void Button8_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button8);
+        }
+        private void Button8_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button8);
+        }
+
+        private void Button8_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button8);
+        }
+
+        #endregion
+
+        #region Button9 Events and Actions
+        private void button9_Click(object sender, EventArgs e)
+        {
+            CheckBulbColor(button9, bulb9);
+        }
+        private void Button9_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMouseEnter(button9);
+        }
+        private void Button9_MouseHover(object sender, EventArgs e)
+        {
+            buttonMouseOver(button9);
+        }
+
+        private void Button9_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMouseLeave(button9);
+        }
+
+        #endregion
+
+        private void CheckBulbColor(Button buttonName, Panel bulbName)
+        {
+            if (bulbName.BackColor != Color.Yellow)
+            {
+                bulbName.BackColor = Color.Yellow;
+            }
+            else
+            {
+                bulbName.BackColor = Color.FromArgb(51, 67, 92);
+            }
+        }
+
+        private void buttonMouseOver(Button buttonName)
+        {
+            buttonName.BackColor = Color.FromArgb(14, 237, 204);
+        }
+
+        private void buttonMouseLeave(Button buttonName)
+        {
+            buttonName.BackColor = Color.FromArgb(70, 132, 227);
+        }
+
+        private void buttonMouseEnter(Button buttonName)
+        {
+            buttonName.BackColor = Color.FromArgb(14, 237, 204);
+        }
+    }
+}
